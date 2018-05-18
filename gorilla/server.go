e8fadc7c747599ee506c5af7e452c1c56498efa0
Mike Atkins <mike@launchdarkly.com>
2018-05-18 11:31:13 -0700

    Allow for the route to be nil
    This appears to happen if you use gorilla to serve static content.

--- gorilla/server.go
+++ gorilla/server.go
@@ -30,8 +30,11 @@ const defaultComponentName = "github.com/gorilla/mux"
 //   r.HandleFunc(pattern, mw)
 func Middleware(tr opentracing.Tracer, h http.Handler, options ...nethttp.MWOption) http.Handler {
 	opNameFunc := func(r *http.Request) string {
-		if tpl, err := mux.CurrentRoute(r).GetPathTemplate(); err == nil {
-			return r.Proto + " " + r.Method + " " + tpl
+		route := mux.CurrentRoute(r)
+		if route != nil {
+			if tpl, err := route.GetPathTemplate(); err == nil {
+				return r.Proto + " " + r.Method + " " + tpl
+			}
 		}
 		return r.Proto + " " + r.Method
 	}
